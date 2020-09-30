43171d567027a80deff7206c495a530f9515c957
Luping <lupinghan@foxmail.com>
2020-09-30 13:52:09 +0800

    update bc.py

--- bc.py
+++ bc.py
@@ -3,13 +3,17 @@
 
 import sys, os, subprocess
 
+scale = "scale=4;"
+
 if len(sys.argv[:]) == 1:
     x = input("calc:")
-    sysip = 'echo "{}" | bc'.format(x)
+    sysip = 'echo "{}{}" | bc'.format(scale, x)
+    #sysip = 'echo "{}" | bc'.format(x)
     os.system(sysip)
 elif len(sys.argv[:]) > 1:
     x = sys.argv[1:]
     for i in x:
-        sysip = 'echo "{}" | bc'.format(i)
+        #sysip = 'echo "{}" | bc'.format(i)
+        sysip = 'echo "{}{}" | bc'.format(scale, i)
         print("{}  = ".format(i))
         subprocess.run(sysip, shell=True)
